7765b8982238697530218bfa718c0fced4b3a4c4
André Herrera <andreherrera97@hotmail.com>
2025-09-19 17:38:00 -0500

    Remove demoOrder object from OrderService to streamline code and improve maintainability

--- src/services/pharmacy/order.ts
+++ src/services/pharmacy/order.ts
@@ -34,30 +34,6 @@ interface UpdatePatientAddressRequest {
   country: string;
 }
 
-const demoOrder = {
-  "patient_id": 1,
-  "physician_id": 1,
-  "ship_to_clinic": 0,
-  "service_type": "two_day",
-  "signature_required": 1,
-  "memo": "Test memo",
-  "external_id": "testing",
-  "test_order": 1,
-  number: 1,
-  "status": "Pending",
-  "stage": "Filling",
-  "products": [
-    {
-      "sku": 1213,
-      "quantity": 30,
-      "refills": 2,
-      "days_supply": 30,
-      "sig": "Use as directed",
-      "medical_necessity": "Patient has a history of diabetes and requires treatment to improve quality of life."
-    }
-  ]
-}
-
 class OrderService {
   private config: PharmacyApiConfig;
 
